f3e76ab4fb06093bcd5b6cca0eb17db0438f38ec
Pedro <pcid@hootap.com>
2017-03-25 17:25:32 +0100

    Test for cell

--- KataScreenshotTests/SuperHeroTableViewCellTests.swift
+++ KataScreenshotTests/SuperHeroTableViewCellTests.swift
@@ -12,6 +12,46 @@ import XCTest
 
 class SuperHeroTableViewCellTests: ScreenshotTest {
 
+    func testShowsAnySuperHero(){
+        
+        let superHero = givenASuperHero(name: "Pedro", description: "Master", avenger: false)
+        
+        let cell = givenASuperHeroTableViewCell()
+        
+        cell.configure(forItem: superHero)
+        
+        verify(view: cell)
+        
+    }
+    func testShowsAvengerSuperHero(){
+    
+        let superHero = givenASuperHero(name: "Pedro", description: "Master", avenger: true)
+        
+        let cell = givenASuperHeroTableViewCell()
+        
+        cell.configure(forItem: superHero)
+        
+        verify(view: cell)
+
+        
+    }
+    func testShowsSuperHeroLongName(){
+        
+        let superHero = givenASuperHero(name: "PedroPedroPedroPedroPedroPedroPedroPedroPedroPedroPedroPedro", description: "Master", avenger: true)
+        
+        let cell = givenASuperHeroTableViewCell()
+        
+        cell.configure(forItem: superHero)
+        
+        verify(view: cell)
+
+    }
+    
+    private func givenASuperHero(name: String, description: String, avenger: Bool) -> SuperHero{
+    
+        return SuperHero(name: name, photo: nil, isAvenger: avenger, description: description)
+    }
+    
     private func givenASuperHeroTableViewCell() -> SuperHeroTableViewCell {
         return UINib(nibName: "SuperHeroTableViewCell", bundle: Bundle.main).instantiate(withOwner: self, options: nil)[0] as! SuperHeroTableViewCell
     }
